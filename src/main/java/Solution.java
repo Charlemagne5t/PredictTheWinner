d221dd7745eec5ee5e18e68b91e8fca1534d5d14
alek <wait82985@gmail.com>
2023-07-28 13:16:38 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,25 @@
 public class Solution {
     public boolean PredictTheWinner(int[] nums) {
-        //TODO
-        return false;
+        Integer[][][] memo = new Integer[nums.length][nums.length][2];
+        return dfs(nums, 0, nums.length - 1, 0, memo) >= 0;
+    }
+
+    private int dfs(int[] nums, int left, int right, int move, Integer[][][] memo) {
+        if (left > right) {
+            return 0;
+        }
+        if (memo[left][right][move] != null) {
+            return memo[left][right][move];
+        }
+
+        int result = 0;
+        if (move == 0) {
+            result = Math.max(dfs(nums, left + 1, right, 1, memo) + nums[left], dfs(nums, left, right - 1, 1, memo) + nums[right]);
+        }
+        if (move == 1) {
+            result = Math.min(dfs(nums, left + 1, right, 0, memo) - nums[left], dfs(nums, left, right - 1, 0, memo) - nums[right]);
+        }
+        memo[left][right][move] = result;
+        return result;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -10,9 +10,43 @@ public class SolutionTest {
         Assert.assertFalse(actual);
     }
 
+
+
     @Test
     public void PredictTheWinnerTestTrue1() {
         int[] nums = {1, 5, 233, 7};
+        boolean actual = new Solution().PredictTheWinner(nums);
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void PredictTheWinnerTestTrue2() {
+        int[] nums = {1, 5, 2, 4, 6};
+        boolean actual = new Solution().PredictTheWinner(nums);
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void PredictTheWinnerTestTrue3() {
+        int[] nums = {1, 1};
+        boolean actual = new Solution().PredictTheWinner(nums);
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void PredictTheWinnerTestTrue4() {
+        int[] nums = {1000, 1000, 1000, 0, 0, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000};
+        boolean actual = new Solution().PredictTheWinner(nums);
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void PredictTheWinnerTestTrue5() {
+        int[] nums = {2, 4, 55, 1};
         boolean actual = new Solution().PredictTheWinner(nums);
 
         Assert.assertTrue(actual);
