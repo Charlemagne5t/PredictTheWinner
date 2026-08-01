5e99accc34c584bcef7c6dbc4b6db2b0c0c5aefc
alek <wait82985@gmail.com>
2026-08-01 16:49:25 +0500

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,25 +1,27 @@
-public class Solution {
-    public boolean PredictTheWinner(int[] nums) {
-        Integer[][][] memo = new Integer[nums.length][nums.length][2];
-        return dfs(nums, 0, nums.length - 1, 0, memo) >= 0;
+import java.util.Arrays;
+
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int total = Arrays.stream(nums).sum();
+        int score1 = dfs(nums, 0, nums.length - 1, 0, 0);
+        int score2 = total - score1;
+        return score1 >= score2 ;
     }
 
-    private int dfs(int[] nums, int left, int right, int move, Integer[][][] memo) {
-        if (left > right) {
-            return 0;
-        }
-        if (memo[left][right][move] != null) {
-            return memo[left][right][move];
+    int dfs(int[] nums, int l, int r, int turn, int score1) {
+        if(l > r) {
+            return score1;
         }
 
-        int result = 0;
-        if (move == 0) {
-            result = Math.max(dfs(nums, left + 1, right, 1, memo) + nums[left], dfs(nums, left, right - 1, 1, memo) + nums[right]);
+        if(turn == 0) {
+            int takeL = dfs(nums, l + 1, r, 1, score1 + nums[l]);
+            int takeR = dfs(nums, l, r - 1, 1, score1 + nums[r]);
+            return Math.max(takeL, takeR);
+        }else {
+            int takeL = dfs(nums, l + 1, r, 0, score1);
+            int takeR = dfs(nums, l, r - 1, 0, score1);
+            return Math.min(takeL, takeR);
         }
-        if (move == 1) {
-            result = Math.min(dfs(nums, left + 1, right, 0, memo) - nums[left], dfs(nums, left, right - 1, 0, memo) - nums[right]);
-        }
-        memo[left][right][move] = result;
-        return result;
+
     }
 }
